2edc625514b42a148d97be77f85501b7f7ddc92f
Allen Lee <6207ALS@gmail.com>
2024-06-13 06:51:45 -0400

    Styled hero page

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,25 @@
+import "./styles.css"
+
 function Hero() {
   return (
 		<div id="hero_container">
-			<h1>Hero</h1>
+			<div id="hero-left_container">
+				<div id="hero-text_container">
+					<h1>Learn and Excel.</h1>
+					<h2>Transform your education with media.</h2>
+				</div>
+				<div id="hero-buttons_container">
+					<button className="hero_button">Watch Videos</button>
+					<button className="hero_button">Learn More</button>
+				</div>
+			</div>
+			<div id="hero-right_container">
+				<div id="hero-blobs_container">
+					<div className="blob" id="blob-aqua"></div>
+					<div className="blob" id="blob-navy"></div>
+					<div className="blob" id="blob-gold"></div>
+				</div>
+			</div>
 		</div>
   )
 }
