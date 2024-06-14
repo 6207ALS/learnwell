728e866006a50421d40833c96b6ee49eccc0bb93
Allen Lee <6207ALS@gmail.com>
2024-06-14 03:58:27 -0400

    Fixed z-index for some pages and added minor styling

--- src/components/MissionBanner/index.tsx
+++ src/components/MissionBanner/index.tsx
@@ -4,12 +4,14 @@ import "./styles.css";
 
 function MissionBanner() {
   return (
-		<div id="mission-banner_container">
-			<div id="mission-banner-text_container">
-				<h1 id="mission-banner-text-1">Quality, Accessibility,</h1>
-				<h1 id="mission-banner-text-2">& Innovation</h1>
+		<>
+			<div id="mission-banner_container">
+				<div id="mission-banner-text_container">
+					<h1 id="mission-banner-text-1">Quality, Accessibility,</h1>
+					<h1 id="mission-banner-text-2">& Innovation</h1>
+				</div>
 			</div>
-		</div>
+		</>
   )
 }
 
